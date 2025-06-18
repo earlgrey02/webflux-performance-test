006fa47817a5361883188208f579d19cf51d21fb
earlgrey02 <san06036@naver.com>
2025-06-19 00:25:57 +0900

    Reactor의 Scheduler를 사용하는 테스트 케이스 추가

--- webflux/src/main/kotlin/com/example/webflux/controller/DemoController.kt
+++ webflux/src/main/kotlin/com/example/webflux/controller/DemoController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import kotlin.math.sqrt
 
 @RestController
@@ -19,13 +20,25 @@ class DemoController(
         repeat(10_000_000) { sqrt(it.toDouble()) }
     }
 
-    @GetMapping("/io/blocking")
+    @GetMapping("/cpu-with-scheduler")
+    fun `CPU Bound with Scheduler`(): Mono<Void> =
+        Mono.fromCallable { `CPU Bound`() }
+            .subscribeOn(Schedulers.parallel())
+            .then()
+
+    @GetMapping("/blocking-io")
     fun `Blocking IO Bound`() {
         demoRepository.findById(1)
     }
 
-    @GetMapping("/io/non-blocking")
+    @GetMapping("/non-blocking-io")
     fun `Non-blocking IO Bound`(): Mono<Void> =
         reactiveDemoRepository.findById(1)
+            .then()
+
+    @GetMapping("/blocking-io-with-scheduler")
+    fun `Blocking IO Bound with Scheduler`(): Mono<Void> =
+        Mono.fromCallable { `Blocking IO Bound`() }
+            .subscribeOn(Schedulers.boundedElastic())
             .then()
 }
