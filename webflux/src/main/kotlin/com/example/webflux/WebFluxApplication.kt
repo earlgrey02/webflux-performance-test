575f15e00f048892e21f4b847359401afd475bc3
earlgrey02 <san06036@naver.com>
2025-06-19 00:26:12 +0900

    이벤트 루프 스레드 개수를 기본값으로 가지도록 수정

--- webflux/src/main/kotlin/com/example/webflux/WebFluxApplication.kt
+++ webflux/src/main/kotlin/com/example/webflux/WebFluxApplication.kt
@@ -2,13 +2,10 @@ package com.example.webflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import reactor.netty.ReactorNetty
 
 @SpringBootApplication
 class WebFluxApplication
 
 fun main(vararg args: String) {
-    System.setProperty(ReactorNetty.IO_WORKER_COUNT, "8")
-
     runApplication<WebFluxApplication>(*args)
 }
